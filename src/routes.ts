51a2a948c30eb3a5885c8ca11203bc0d384ec970
demostenesbr <demostenes@demostenes.com.br>
2024-06-20 23:27:59 -0300

    Feat: first commit and start Swagger

---
+++ src/routes.ts
@@ -0,0 +1,71 @@
+import { Router } from "express";
+import { v4 as uuidv4 } from 'uuid';
+import { ensuredAuthenticated } from "./middleware";
+
+const router = Router();
+
+interface ProductsDTO {
+    name: string;
+    description: string;
+    price: number;
+    id: string;
+}
+
+const products: ProductsDTO[] = [];
+
+router.get("/products/findByName", (request, response) => {
+    const { name } = request.query;
+    const product = products.filter((p) => p.name.includes(String(name)));
+    return response.json(product);
+});
+
+router.get("/prodcts/:id", (request, response) => {
+    const { id } = request.params;
+    const product = products.find((p) => p.id === id);
+    return response.json(product);
+});
+
+router.post("/products", ensuredAuthenticated, (request, response) => {
+    const { name, description, price } = request.body;
+
+    const productAlreadyExists = products.find(
+        (product) => product.name === name
+    );
+
+    if (productAlreadyExists) {
+        return response.status(400).json({ error: "Product already exists!" });
+    }
+    
+    const product: ProductsDTO = {
+        name,
+        description,
+        price,
+        id: uuidv4(),
+    };
+
+    products.push(product);
+    return response.json(product);
+});
+
+router.put("/products/:id", ensuredAuthenticated, (request, response) => {
+    const { id } = request.params;
+    const { name, description, price } = request.body;
+
+    const productIndex = products.findIndex((p) => p.id === id);
+
+    if (productIndex === -1) {
+        return response.status(400).json({ error: "Product doesn't exists!" });
+    }
+
+    const product: ProductsDTO = Object.assign({
+        name,
+        description,
+        price,
+    });
+
+    products[productIndex] = product;
+
+    return response.json(product);
+});
+
+export { router };
